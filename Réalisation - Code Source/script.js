8f0bc2fd1caf18bf9a4cce76cf70a543477cad39
AKHRIFJaafar <Akhrif.jaafar.dev@gmail.com>
2021-06-24 16:06:21 +0100

    Fixé close

--- Réalisation - Code Source/script.js
+++ Réalisation - Code Source/script.js
@@ -1,21 +1,4 @@
-// return (
-//   <div className="container">
-//     <div className="row">
-//       <div className="col-sm-6 col-sm-offset-3 m-auto">
-//         <h1> Tâches à faire</h1>
-//         <form
-//           id="form-add"
-//           className="form-horizontal">
-//           <div className="input-group w-100">
-//             <input type="text" className="form-control" placeholder="Description de la tâche..." aria-label="Input group example" aria-describedby="basic-addon1"/>
-//               <button className="btn btn-default">                 
-//                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-square" viewBox="0 0 16 16">
-//                 <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
-//                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
-//               </svg>
-//             </button>
-// </div>
-//         </form>
+
 // Composant : Tâche
 class Task extends React.Component {
   constructor(props) {
@@ -29,7 +12,9 @@ class Task extends React.Component {
     return (
       <div className={class_name} onClick={this.props.onClickTask}>
         <span>{this.props.value}</span>
-        <i className="close" onClick={this.props.onClickClose}>&times;</i>
+        <i className="close" onClick={this.props.onClickClose}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+</svg></i>
       </div>
     )
   }
