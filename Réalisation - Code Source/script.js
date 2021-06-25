20a828e6f5b443ace544a20fa37aabc27647dae3
AKHRIFJaafar <Akhrif.jaafar.dev@gmail.com>
2021-06-25 15:52:48 +0100

    ajouter réalisation

--- Réalisation - Code Source/script.js
+++ Réalisation - Code Source/script.js
@@ -9,7 +9,7 @@ class Task extends React.Component {
     class_name += this.props.done == 1 ? ' task-success' : ' task-info';
 
     return (
-              <div className={class_name} onClick={this.props.onClickTask}>
+         <div className={class_name} onClick={this.props.onClickTask}>
         <span>{this.props.value}</span>
         <i className="close" onClick={this.props.onClickClose}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
           <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
@@ -127,9 +127,13 @@ class App extends React.Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 m-auto">
+          <div className=" m-auto">
+            <div>
             <h1 id="logo"> Todo</h1>
+            </div>
+            <div className="just">
             {tasksArray}
+            </div>
             <form
               id="form-add"
               className="form-horizontal"
